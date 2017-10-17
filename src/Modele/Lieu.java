407659f68668eb3f21c59ee1e56fa29965a3cfc9
etien <etien@192.168.0.26>
2017-10-17 17:54:03 +0100

    classe Icone

--- src/Modele/Lieu.java
+++ src/Modele/Lieu.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
  *
  * @author Léa
  */
-public class Lieu {
+public abstract class Lieu {
     //Atribut
     private Icone icone;
     private ArrayList<Lieu> prerequis;
