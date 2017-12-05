7ed316fa7ebfa01ff054e7dca624e77636732b8c
Rhododendron1210 <lea.louesdon@gmail.com>
2017-12-05 19:14:43 +0100

    classe conxion de la bd

---
+++ src/Controleur/Connexion.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controleur;
+import java.sql.*;
+/**
+ *
+ * @author Léa
+ */
+public class Connexion {
+    
+    public static void main(String[] args) {   
+        
+    }
+    public static Connection ConnexterDB(){
+    try {
+      Class.forName("com.mysql.jdbc.Driver"); /* chemin du driver*/
+      System.out.println("Driver O.K.");
+
+      String url = "jdbc:postgresql://localhost:5432/Ecole"; /* url a changer en fonctio de la base 
+      2-> localisation de la machine physique sur le reseau plus port
+      3-> nom de la base de données*/
+      String user = "postgres";
+      String passwd = "postgres";
+
+      Connection conn = DriverManager.getConnection(url, user, passwd);
+      System.out.println("Connexion effective !");   
+      return conn;
+    } catch (Exception e) {
+      e.printStackTrace();
+      return null;
+    }      
+  }
+}
+
