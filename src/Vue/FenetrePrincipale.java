4c18c34ead0a24bb917b9e6c536afd65ff7045ae
Rhododendron1210 <lea.louesdon@gmail.com>
2017-12-08 16:13:06 +0100

    fenetre principale

--- src/Vue/FenetrePrincipale.java
+++ src/Vue/FenetrePrincipale.java
@@ -46,12 +46,12 @@ public class FenetrePrincipale extends JFrame implements Observateur {
         //Panel du haut 
         panelHaut = new JPanel();
         panelHaut.setBackground(Color.WHITE);
-        mascotte = new JLabel("masotte");
+        /*mascotte = new JLabel("masotte");
         mascotte.setBorder(BorderFactory.createLineBorder(Color.black));
         panelHaut.add(mascotte, BorderLayout.WEST);
         message = new JLabel("/////////////////message a modifier//////////////////////");
         message.setBorder(BorderFactory.createLineBorder(Color.black));
-        panelHaut.add(message, BorderLayout.CENTER);
+        panelHaut.add(message, BorderLayout.CENTER);*/
        
         //bouton menu
         JButton menu = new JButton("menu");
