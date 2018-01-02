d1fb3a1ecce1ec357aa7a2cefa4ac6b531200dbb
Franck <Franck@192.168.86.151>
2018-01-02 11:27:56 +0100

    orthographe

--- src/Controleur/Controleur.java
+++ src/Controleur/Controleur.java
@@ -79,14 +79,14 @@ public class Controleur implements Observateur {
         /////////////////////////////////MONDE///////////////////////////////////////
         Icone icone = new Icone((float) 0.05, (float) 0.2, "images/mondeCuisiniers.png", 300, 450);
         Carte mondeMedievale = new Carte(icone, "Ferte Ylia", "images/placeMarche.jpg");
-        mondeMedievale.setDescriptif("<html>C'est dans ce monde qui tu trouveras les meilleurs ingrédients ! Et des cuisiniers à leurs hauteurs ...</html>");
+        mondeMedievale.setDescriptif("<html>C'est dans ce monde que tu trouveras les meilleurs ingrédients ! Et des cuisiniers à leur hauteur ...</html>");
         
         icone = new Icone((float) 0.3, (float) 0.1, "images/mondeArcheologue.png", 350, 400);
-        Carte mondeArcheologue = new Carte(icone, "Monde des Archeologue", "images/mondeA.jpg");
-        mondeArcheologue.setDescriptif("<html> Si vous suportez la poussiere, <br> et retournez la terre, <br>vous trouverez trésors, <br> et bien plus encore.</html>");
+        Carte mondeArcheologue = new Carte(icone, "Monde des Archeologues", "images/mondeA.jpg");
+        mondeArcheologue.setDescriptif("<html> Si vous supportez la poussière, <br> et retournez la terre, <br>vous trouverez trésors, <br> et bien plus encore.</html>");
         icone = new Icone((float) 0.6, (float) 0.3, "images/mondeLasVegas.png", 350, 400);
         Carte MondeLasVegas = new Carte(icone, "Le Refuge", "images/mondeLasVegas.jpg");
-        MondeLasVegas.setDescriptif("<html>La gaité n'est qu'apparente, mais l'espoir subsiste... Lorena a trouvée ce monde, pour tout ceux qui ont perdu le leur.</html>");
+        MondeLasVegas.setDescriptif("<html>La gaïté n'est qu'apparente, mais l'espoir subsiste... Lorena a trouvé ce monde, pour tout ceux qui ont perdu le leur.</html>");
         monde.addContien(mondeMedievale);
         monde.addContien(mondeArcheologue);
         monde.addContien(MondeLasVegas);
@@ -97,27 +97,27 @@ public class Controleur implements Observateur {
         EnigmeComposite andreLePatissier = new EnigmeComposite(icone, "André le Boulanger", "images/vueJeu.png");  
         andreLePatissier.setDescriptif("<html>Ma famille et moi même sommes boulanger-pâtissier depuis des générations ! Je travaille avec les fournisseurs de notre monde pour vous offrir mes plus belles créations ! <br> Et si vous, Merlin ou un autre monde avez besoin de mes talents, n'hésitez pas ! </html>");
         mondeMedievale.addContien(andreLePatissier);
-        Carte ivanLePaysan = new Carte(new Icone((float) 0.75, (float) 0.35, null, 300, 200),"Ivan Le Paysant",null);
-        ivanLePaysan.setDescriptif("<html>Aaaaah... avoir la main verte... C'est innée chez moi, l'amour de tout ce qui pousse. Le respect des terres et des plantes, c'est le secret pour faire pousser les meilleurs ingrédients, pour les meilleurs cuisiniers !</html>");
+        Carte ivanLePaysan = new Carte(new Icone((float) 0.75, (float) 0.35, null, 300, 200),"Ivan Le Paysan",null);
+        ivanLePaysan.setDescriptif("<html>Aaaaah... avoir la main verte... C'est inné chez moi, l'amour de tout ce qui pousse. Le respect des terres et des plantes, c'est le secret pour faire pousser les meilleurs ingrédients, pour les meilleurs cuisiniers !</html>");
          mondeMedievale.addContien(ivanLePaysan);
         //EnigmeChemin psoupe = new EnigmeChemin(new Icone((float) 0.10, (float) 0.39, null, 200, 200), "Les Machines a sous", null,2,3,1);
         //mMedie.addContien(psoupe);
         EnigmeChampsDeTexte berengereLaBergere =new EnigmeChampsDeTexte(new Icone((float) 0.10, (float) 0.39, null, 200, 200),"Bérengere la bergere","images/prés_final_couleur.jpg");
-        berengereLaBergere.setDescriptif("<html>J'adore l'air de la montagne. Les paturages à pertes de vue, l'odeur des sous-bois, marcher au milieu des moutons ... <br>Tu devrais faire une nuit à la belle étoile au milieu des moutons un jour, y'a rien de plus reposant !</html>");
+        berengereLaBergere.setDescriptif("<html>J'adore l'air de la montagne. Les paturages à perte de vue, l'odeur des sous-bois, marcher au milieu des moutons ... <br>Tu devrais faire une nuit à la belle étoile au milieu des moutons un jour, y'a rien de plus reposant !</html>");
         mondeMedievale.addContien(berengereLaBergere);
         //monde des archéologues
         icone = new Icone((float) 0.40, (float) 0.35, null, 300, 200);
         EnigmeComposite porteDeLaPyramide = new EnigmeComposite(icone, "La Porte de La Pyramide", "images/enigme 1.jpg");
-        porteDeLaPyramide.setDescriptif("<html>C'est très dur d'avancer dans cette pyramide.Ceux qui vivaient ici étaient sans doute de brillant mathématiciens. On ne peut avancer qu'en resolvant des éngimes.  </html>");
+        porteDeLaPyramide.setDescriptif("<html>C'est très dur d'avancer dans cette pyramide. Ceux qui vivaient ici étaient sans doute de brillants mathématiciens. On ne peut avancer qu'en resolvant des éngimes.  </html>");
         mondeArcheologue.addContien(porteDeLaPyramide);
         
         //les fouilles
         Carte lesFuilles = new Carte(new Icone((float) 0.05, (float) 0.55, null, 300, 200),"Fouilles abandonnées",null);
-        lesFuilles.setDescriptif("<html>Ces sites de fouilles ont été abandonnés suite à une invasion de scorpion. Si tu souhaite continuer à fouiller va falloir que tu comprenne leur comportement !</html>");
+        lesFuilles.setDescriptif("<html>Ces sites de fouilles ont été abandonnés suite à une invasion de scorpions. Si tu souhaites continuer à fouiller, il va falloir que tu comprennes leur comportement !</html>");
         mondeArcheologue.addContien(lesFuilles);
         //la grotte
         Carte laGrotte = new Carte(new Icone((float) 0.75, (float) 0.50, null, 300, 200),"Grotte",null);
-        laGrotte.setDescriptif("<html>Voici les grottes, elles sont truffées de pièges, si tu veux les eviter il va te falloir un compagon programmable afin de passer dans les endroits les plus étriqués. </html>");
+        laGrotte.setDescriptif("<html>Voici les grottes, elles sont truffées de pièges, si tu veux les éviter il va te falloir un compagnon programmable afin de passer dans les endroits les plus étriqués. </html>");
         mondeArcheologue.addContien(laGrotte);
         //Monde de LasVegas
         
@@ -126,7 +126,7 @@ public class Controleur implements Observateur {
         MondeLasVegas.addContien(machines);
         //le poulpe
         Carte lePoulpe = new Carte(new Icone((float) 0.30, (float) 0.15, null, 300, 200),"Grasvz'in",null);
-        lePoulpe.setDescriptif("<html>Le Refuge... Ce monde porte bien son nom. Peu nombreux sont ceux qui viennent vivre ici de leur plein grès. Ceux que tu vois autour de toi ont tout perdu. Lorena et son ancien apprenti nous on sauvés lorsque les déstructeurs sont venu dans nos mondes, les réduisants à l'état de débris. La joie et l'amusement ne sont qu'une facade, c'est la tristesse et la nostalgie qui s'emparent de nos coeur... Jamais nous ne retrouverons nos foyers. <br> L'air empeste la peur, les destructeurs, avec Vagnar à leur tête, reviendront plus puissant que jamais. Soyez à la hauteur, et faites les bon choix ...<html>");
+        lePoulpe.setDescriptif("<html>Le Refuge... Ce monde porte bien son nom. Peu nombreux sont ceux qui viennent vivre ici de leur plein grès. Ceux que tu vois autour de toi ont tout perdu. Lorena et son ancien apprenti nous ont sauvés lorsque les destructeurs sont venus dans nos mondes, les réduisants à l'état de débris. La joie et l'amusement ne sont qu'une facade, c'est la tristesse et la nostalgie qui s'emparent de nos coeurs... Jamais nous ne retrouverons nos foyers. <br> L'air empeste la peur, les destructeurs, avec Vagnar à leur tête, reviendront plus puissants que jamais. Soyez à la hauteur, et faites les bons choix ...<html>");
         MondeLasVegas.addContien(lePoulpe);
         ///////////////////////////////ENIGMES/////////////////////////////////////
         ////////////////////////////////HISTOIRE////////////////////////////////////
